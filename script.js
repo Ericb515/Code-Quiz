5aab9f6d1d1e764344e0f5c81368790a77a54e51
Eric Boatwright <ericboatwright9@gmail.com>
2021-03-22 18:54:07 -0500

    new cahneges to  JS

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-//function : Start quiz
-// function: on click start button
-// setInterval timer
-// <div class ="question" />
-        // <h1>
-// on click  for any questions
-    //"arrays"
-     // var questions=[ ]
-// var timer = 5 or 10 sec
-       //(time --) 
-// to clear timer is clearInterval for either or 
-// local storage is to save info locally 
-// .set item
-// .get item 
-     
+function startQuiz(p) {
+    theStartButton
+  }
+  function{
+      
+  }
+  var questions [ =
+  {
+      prompt:"What Is not an type coding language?/n(a) Love\n\ 
+      (b) Javascript\n(c) All Of the Above" ,
+  answer:"a"
+  }
+  ]
