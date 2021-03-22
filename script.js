cdfe0b74b90410f3eb6f62e90bf0f5d48e4256c1
Eric Boatwright <ericboatwright9@gmail.com>
2021-03-22 14:30:07 -0500

    new comments

---
+++ script.js
@@ -0,0 +1,15 @@
+//function : Start quiz
+// function: on click start button
+// setInterval timer
+// <div class ="question" />
+        // <h1>
+// on click  for any questions
+    //"arrays"
+     // var questions=[ ]
+// var timer = 5 or 10 sec
+       //(time --) 
+// to clear timer is clearInterval for either or 
+// local storage is to save info locally 
+// .set item
+// .get item 
+     
